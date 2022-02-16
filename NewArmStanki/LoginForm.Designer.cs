ed83e7fc70481d15d79a5f06fd714f46a2479e14
sana-love-cs sana-love-cs <sana-ivanov1@yandex.ru>
2022-02-16 21:34:36 +0500

    upd

--- NewArmStanki/LoginForm.Designer.cs
+++ NewArmStanki/LoginForm.Designer.cs
@@ -103,6 +103,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(214, 72);
             this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // pictureBox2
             // 
