9c740580d5fbc956e1a20a66a92b011cc24ed047
sana-love-cs sana-love-cs <sana-ivanov1@yandex.ru>
2022-02-17 20:46:07 +0500

    NEW

--- NewArmStanki/DB.cs
+++ NewArmStanki/DB.cs
@@ -9,7 +9,24 @@ namespace NewArmStanki
 {
     internal class DB
     {
-        MySqlConnection connection = new MySqlConnection("server=");
+        MySqlConnection connection = new MySqlConnection("server=localhost;username=root;database=test");
+
+         public void openConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed) //Если не подклчена
+                connection.Open();  //Подключаюсь к бд
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) //Если подключена
+                connection.Close();  //Отключаюсь к бд
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
 
     }
 }
